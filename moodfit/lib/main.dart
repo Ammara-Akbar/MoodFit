bb51e7187b4a3d4e6b7e5cea060afd04aca80e0e
Ammara Akbar <107490940+Ammara-Akbar@users.noreply.github.com>
2025-10-22 04:14:48 -0700

    firebase integration

--- moodfit/lib/main.dart
+++ moodfit/lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:moodfit/screens/web/web_login_screen.dart';
 import 'package:moodfit/splash_screen.dart';
 
-void main() {
+Future <void> main() async{
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+
   runApp(const MyApp());
-}
+}      
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,8 +19,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
      
-      home: WebLoginScreen()
-      //  SplashScreen(),
+      home:
+      //  WebLoginScreen()
+      SplashScreen(),
     );
   }
 }
